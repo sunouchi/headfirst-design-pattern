c730a9afb597435f39b78e1051d867bec80a51d3
sunouchi-t <sunouchi-t@kadokawa.jp>
2017-11-16 18:28:39 +0900

    Add 03

---
+++ 03/Beverage.java
@@ -0,0 +1,22 @@
+public class Bevarage {
+    str description = "最高級ダークロースト";
+    double cost = 280;
+
+    public double cost() {
+        return cost;
+    }
+}
+
+public DarkRoast() {
+    double cost = 350;
+    Size size = 
+
+    public double cost() {
+    }
+}
+
+public interface Size {
+}
+
+public class Small implements Size {
+}
